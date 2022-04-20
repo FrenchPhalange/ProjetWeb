89fe10638cf50d87e5bdb3ab94dc49a2fddac3c8
Romain Cian <romain.cian@lidem.education>
2022-04-20 15:26:56 +0200

    test page piscine

--- pages/piscine.php
+++ pages/piscine.php
@@ -5,7 +5,191 @@ include_once "../template/header.php";
 ?>
 
 <main>
+    <!-- SECTION 2 -->
+    <section id="intro"  class="section section-2">
+        <img src="../img/piscine.jpg" alt="" />
+        <div class="left-section-2">
+        <p class="title-section-2">Piscine</p>
+        <p class="subtitle-section-2">
+            Venez rejoindre nos cours de water bike.
+            Tout les --, de --H à --H, Vener nombreux
+            pour ---- et ----. Vener pédaler et nager
+            avec nous, vous verrez c’est amusant !
+        </p>
+        <button class="read-more">lire la suite</button>
+        </div>
+    </section>
+    <!-- SECTION 2 -->
 
+    <!-- SECTION 3 -->
+    <section id="infos" class="section section-3">
+        <div class="right-section-3">
+        <div class="all-cards">
+            <div class="part-1-section-3" data-aos="fade-up-right">
+            <div class="card-info-page2">
+                <img src="../img/coach4.jpg" alt="" />
+                <p class="title-card-info">Léo Ordman</p>
+                <p class="description-card-info">
+                - Diplomé par l’Etat <br>
+                - Spécialité : Natation & Water Polo
+                </p>
+            </div>
+            </div>
+            <div class="part-2-section-3" data-aos="fade-up-right">
+            <div class="card-info-page2">
+                <img src="../img/coach5.jpg" alt="" />
+                <p class="title-card-info">Scott McKenzie</p>
+                <p class="description-card-info">
+                -Diplomé par l’Etat <br>
+                -Specialité: Natation & Musculation
+                </p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- SECTION 3 -->
+    <!-- SECTION 4 -->
+    <section id="intro"  class="section section-2">
+        <img src="../img/piscine2.jpg" alt="" />
+        <div class="left-section-2">
+        <p class="title-section-2">Notre piscine</p>
+        <p class="subtitle-section-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Tellus pellentesque felis gravida sed integer.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Tellus pellentesque felis gravida sed integer.
+        </p>
+        <button class="read-more">lire la suite</button>
+        </div>
+    </section>
+    <!-- SECTION 4 -->
+    <!-- SECTION 5 -->
+    <section id="discover" class="section section-5">
+        <p class="title-section-5">Le Planning</p>
+        <table>
+          <caption>      *Tout les Lundis et Samedis de --H à --H, sauf jour férié
+        </caption>
+        <tr>
+            <th scope="col"></th>
+                <th scope="col">Lundi</th>
+                <th scope="col">Mardi</th>
+                <th scope="col">Mercredi</th>
+                <th scope="col">Jeudi</th>
+                <th scope="col">Vendredi</th>
+                <th scope="col">Samedi</th>
+                <th scope="col">Dimanche</th>
+            </tr>
+            <tr>
+                <th scope="row">9h-10h</th>
+                <td class="coach4">Léo</td>
+                <td></td>
+                <td class="coach5">Scott</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="coach4">Léo</td>
+            </tr>
+            <tr>
+                <th scope="row">10h-11h</th>
+                <td class="coach4">Léo</td>
+                <td></td>
+                <td class="coach5">Scott</td>
+                <td class="coach4">Léo</td>
+                <td></td>
+                <td class="coach5">Scott</td>
+                <td class="coach4">Léo</td>
+            </tr>
+            <tr>
+                <th scope="row">11h-12h</th>
+                <td></td>
+                <td ></td>
+                <td></td>
+                <td class="coach4">Léo</td>
+                <td></td>
+                <td class="coach5">Scott</td>
+                <td></td>
+            </tr>
+            <tr>
+            <th scope="row">*</th>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+        </tr>
+        <tr>
+            <th scope="row">*</th>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+            <td>*</td>
+        </tr>
+        <tr>
+            <th scope="row">15h-16h</th>
+            <td ></td>
+            <td></td>
+            <td class="coach4">Léo</td>
+            <td></td>
+            <td class="coach5">Scott</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">16h-17h</th>
+            <td></td>
+            <td class="coach5">Scott</td>
+            <td class="coach4">Léo</td>
+            <td></td>
+            <td class="coach5">Scott</td>
+            <td class="coach4">Léo</td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">17h-18h</th>
+            <td></td>
+            <td class="coach5">Scott</td>
+            <td></td>
+            <td></td>
+            <td class="coach5">Scott</td>
+            <td class="coach4">Léo</td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">18h-19h</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="coach4">Léo</td>
+            <td></td>
+        </tr>
+    </table>
+    </section>
+    <!-- SECTION 5 -->
+    <!-- SECTION 6 -->
+    <section id="intro"  class="section section-page2">
+        <div class="left-section-page2">
+        <p class="title-section-page2">Inscription</p>
+        <p class="subtitle-section-page2">
+            Si vous souhaiter vous inscrire pour le prochain cours de fitness, c’est ici:
+        </p>
+        <button class="read-more-page2">Mardi</button>
+        <button class="read-more-page2">Vendredi</button>
+        <p class="subtitle-section-page22">
+            Place restantes : <br>
+            Lundi : 8/ 20 <br>
+            Samedi : 18 / 20 <br>
+        </p>
+        </div>
+    </section>
+    <!-- SECTION 6 -->
 </main>
 
 <script src="../js/piscine.js"></script>
